43917e5839fc03a02e4968ffb9b277884793261a
myroman-wuLtCL <myroman@protonmail.com>
2023-05-23 22:25:12 -0400

    more components started

---
+++ src/components/contact.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Contact() {
+
+    return(
+
+    )
+}
+export default Contact;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Nav from './nav';
+
+function Header() {
+    return (
+
+    )
+}
+
+export default Header;
